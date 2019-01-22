a6cd1ea068a2405de57743c9d8f3bcacac84d2ba
ttiagoestevaoo <43210017+ttiagoestevaoo@users.noreply.github.com>
2019-01-22 00:09:04 -0200

    Testando
    é sso

---
+++ jogoquiz/src/jogoquiz/alternativas.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogoquiz;
+
+/**
+ *
+ * @author internet
+ */
+public class alternativas {
+    private String alternativa;
+    private boolean alternativaCorreta;        
+}
